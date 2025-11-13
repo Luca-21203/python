c1902614746889c8150d3c06011c2fc0d3fdec63
Luca-21203 <lgelmetti21203@iesjoanramis.org>
2025-11-13 10:50:06 +0000

    Ex11

---
+++ ex12.py
@@ -0,0 +1,8 @@
+edat = int(input("Escriu la seva edat: "))
+if edat > 18:
+    print("És major d'edat.")
+elif edat<18:
+    print("És menor d'edat.")
+else:
+    print("Enhorabona: aquest any has fet 18 anys.")
+
