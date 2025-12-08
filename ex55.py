e6b43e2e4c8f621e7831b795df3df2e335501dca
Luca21203 <lgelmetti21203@iesjoanramis.org>
2025-12-08 21:55:15 +0000

    Create ex55.py

---
+++ ex55.py
@@ -0,0 +1,11 @@
+# Mostrar números parells fins a 100
+print("Números parells fins a 100:")
+for num in range(2, 101, 2):
+    print(num, end=" ")
+
+print("\n")
+
+# Mostrar números senars fins a 100
+print("Números senars fins a 100:")
+for num in range(1, 100, 2):
+    print(num, end=" ")
