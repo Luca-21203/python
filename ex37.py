117c8aeab7f62d9acfbbb296ef45375febda75f1
Luca21203 <lgelmetti21203@iesjoanramis.org>
2025-12-08 21:35:28 +0000

    Create ex37.py

---
+++ ex37.py
@@ -0,0 +1,63 @@
+import random
+import time
+
+# Funció on expliquem què passa
+def intro():
+    print ("""En una època on els gegants governen Menorca. Nosaltres necessitem menjar,
+    Estem seguint el rastre de l'olor del menjar, però ens trobem en una cruïa.
+    Al final de cada camí hi ha un talaiot, en un viuen els gegants bons que ens convidaran
+    i en l'altre són uns caníbals afamats, i ens menjaran just ens vegin.
+    """)
+
+# Funció on demanem a quin talaiot volem anar
+def canviTalaiot():
+    talaiot = ""
+    while talaiot != "1" and talaiot != "2":
+        talaiot = input("A quin Talaiot vols anar? Introdueixi 1 o 2: ")
+    return talaiot
+
+# Funció que ens indica si guanyem o perdem
+def trobada(canviTalaiot):
+    print ("T'estas apropant al talaiot...")
+    time.sleep(2)
+    print ("Està fosc i és tenebrós...")
+    time.sleep(2)
+    print ("Un gran gegant salta davant teu, t'agafa i ...")
+    print ("")
+    time.sleep(2)
+
+    gegantamic = random.randint(1, 2)
+
+    if canviTalaiot == str(gegantamic):
+        print ("Et convida a menjar...")
+        return True      # GUANYES
+    else:
+        print ("Se't menja d'un mos... ÑAM ÑAM ÑAM")
+        return False     # PERDS
+
+
+# ───────── PROGRAMA PRINCIPAL ─────────
+
+partidaNova = "si"
+
+while partidaNova == "s" or partidaNova == "si":
+
+    punts = 0
+    seguim = True
+
+    while seguim:
+        intro()
+        nTalaiot = canviTalaiot()
+        victòria = trobada(nTalaiot)
+
+        if victòria:
+            punts += 1
+            print("Has guanyat! Punts acumulats:", punts)
+            seguim = True
+        else:
+            print("Has perdut!")
+            print("Punts totals aconseguits:", punts)
+            seguim = False
+
+    partidaNova = input("Vols tornar a jugar? Introdueixi si o no: ")
+    print("\n")
