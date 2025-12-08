85f93af9e388975b8c8ffdfc4442783d4c53c149
Luca21203 <lgelmetti21203@iesjoanramis.org>
2025-12-08 21:22:01 +0000

    Update ex26.py

--- ex26.py
+++ ex26.py
@@ -1,18 +1,26 @@
-# Funció per retornar el número més gran d'una llista
 def gran_llista(llista):
-    """
-    Retorna el número més gran d'una llista de nombres.
-    """
-    if not llista:  # Comprovem si la llista està buida
-        return None
-    max_valor = llista[0]
-    for num in llista:
-        if num > max_valor:
-            max_valor = num
-    return max_valor
+    major = llista[0]
+    for x in llista:
+        if x > major:
+            major = x
+    return major
 
-# Proves
-print(gran_llista([3, 4, 2, 3, 10]))  # 10
-print(gran_llista([5, 5, 5, 5]))      # 5
-print(gran_llista([-2, -7, -1, -9]))  # -1
-print(gran_llista([]))                 # None
+
+# Demanar la llista a l'usuari
+llista = []
+print("Introdueix números enters. Escriu 'fi' per acabar:")
+
+while True:
+    valor = input("Número: ")
+
+    if valor == "fi":
+        break
+
+    llista.append(int(valor))
+
+
+# Mostrar el resultat
+if len(llista) > 0:
+    print(gran_llista(llista))
+else:
+    print("La llista està buida")
