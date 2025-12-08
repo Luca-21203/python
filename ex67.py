9ca5517d8d6a224a19be001cb7853e875b9d0f6e
Luca21203 <lgelmetti21203@iesjoanramis.org>
2025-12-08 22:10:03 +0000

    Create ex67.py

---
+++ ex67.py
@@ -0,0 +1,8 @@
+# Demanar una paraula a l'usuari
+paraula = input("Introdueix una paraula: ")
+
+# Crear la llista amb list comprehension
+llista_lletres = [lletra for lletra in paraula]
+
+# Mostrar el resultat
+print(llista_lletres)
